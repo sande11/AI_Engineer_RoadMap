a56277fa26a785719fa09e7784f68c54ff7ed63c
Kelvin Sande <98394184+sande11@users.noreply.github.com>
2026-04-12 21:09:52 +0200

    Add string methods and examples to strings.py; create conditional_statements.py and math_operators.py with basic examples

---
+++ python_basics/conditional_statements.py
@@ -0,0 +1,26 @@
+# if statements
+# x = 1
+# if (x == 1):
+#     print('true')
+
+# a = 5
+# b = 10
+
+# if (a > b):
+#     print('a is greater than b')
+# else:
+#     print('a is less than b')
+
+# multiple conditions
+
+c = 10
+d = 23
+
+if ((c > 23) and (d > 25)):
+    print('both them numbers are greater than 20')
+
+elif ((c == 10) and (d == 23)):
+    print('c and d match the expected values')
+
+else:
+    print('no they are not')
--- python_basics/strings.py
+++ python_basics/strings.py
@@ -8,14 +8,42 @@ age = 30
 
 #String concatenation
 conc = name + "is " + str(age) + " years old."
-print(conc)
+# print(conc)
 
 #String formatting
 #used to insert variables into a string
 
 #arguments by position
-print ("My name is {name} and I am {age}". format(name = name, age = age))
+# print ("My name is {name} and I am {age}". format(name = name, age = age))
 #f-strings (formatted string literals)
-print (f'Hello, my name is {name} and i am {age}')
+# print (f'Hello, my name is {name} and i am {age}')
 
 #string methods
+#string methods are built-in functions you can call on a string using dot notation.
+"hello".upper()       # 'HELLO'
+"HELLO".lower()       # 'hello'
+"python".capitalize() # 'Python'
+"python rocks".title()# 'Python Rocks'
+"PyThOn".swapcase()   # 'pYtHoN'
+
+"  hi  ".strip()      # 'hi'
+"  hi  ".lstrip()     # 'hi  '
+"  hi  ".rstrip()     # '  hi'
+
+"hello world".replace("world", "Python")   # 'hello Python'
+
+"abc".isalpha()      # True
+"123".isdigit()      # True
+"abc123".isalnum()   # True
+"hello".islower()    # True
+"HELLO".isupper()    # True
+"   ".isspace()      # True
+
+"hello".find("e")         # 1
+"hello".index("e")        # 1
+"hello".startswith("he")  # True
+"hello".endswith("lo")    # True
+"banana".count("a")       # 3
+
+school = 'kasungu demo'
+print(school.upper([0]))
