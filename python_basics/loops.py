b33233f7a548877c77f85c310609b51f00ef4d07
Kelvin Sande <98394184+sande11@users.noreply.github.com>
2026-06-03 15:29:11 +0200

    Add examples for while and for loops, including break, continue, and range functions in loops.py

--- python_basics/loops.py
+++ python_basics/loops.py
@@ -40,3 +40,51 @@ while y < 9:
 # 7
 # 8
 # 9
+
+# With the else statement we can run a block of code once when the condition no longer is true:
+i =1
+while i < 9:
+    print(i)
+    i += 1
+else:
+    print("no match")
+
+# Python For Loops
+# A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
+# This is less like the for keyword in other programming languages, and works more like an iterator method as found in other object-orientated programming languages.
+# With the for loop we can execute a set of statements, once for each item in a list, tuple, set etc.
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+# With the break statement we can stop the loop before it has looped through all the items:
+
+# Example
+# Exit the loop when x is "banana":
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+  
+#   With the continue statement we can stop the current iteration of the loop, and continue with the next:
+# Example
+# Do not print banana:
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+
+#   The range() Function
+# To loop through a set of code a specified number of times, we can use the range() function,
+# The range() function returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and ends at a specified number.
+
+for x in range(6):
+  print(x)
+
+for x in range(2, 6):
+  print(x)
