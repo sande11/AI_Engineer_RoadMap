da6a5c8c8825d2b2ed0f57c532d327b25d4a68e7
Kelvin Sande <98394184+sande11@users.noreply.github.com>
2026-06-05 21:07:27 +0200

    Add examples of function definitions and usage in functions.py

---
+++ python_basics/functions.py
@@ -0,0 +1,20 @@
+# reusable code
+
+def greet():
+    print("hi there")
+    print("welcome aboard")
+
+
+greet()
+# passing arguments
+def greet( first_name, last_name):
+    # the f is for comverting to a formatted string
+    print(f"hi {first_name} {last_name}")
+    print("welcome aboard")
+
+
+greet("kelvin", "sande")
+
+# types of functions
+# performs a task
+# returns a value
